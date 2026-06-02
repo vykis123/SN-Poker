d88984dd8f7bf38c24d40a4704cdcc22b8ba62e6
System Administrator <admin@example.com>
2026-06-02 14:50:29 +0300

    Logo updated to anchor

--- src/fluent/Theme/header/header.now.ts
+++ src/fluent/Theme/header/header.now.ts
@@ -6,7 +6,9 @@ SPHeaderFooter({
     htmlTemplate: `
         <nav class="nav">
             <div class="logo">
-                <img src="x_872084_sn_poker.sn-poker-logo.svg" alt="SN Poker Image" width="164" height="32" />
+                <a href="/sn_poker" aria-label="Navigate back to home page">
+                    <img src="x_872084_sn_poker.sn-poker-logo.svg" alt="SN Poker Image" width="164" height="32" />
+                </a>
             </div>
         </nav>
     `,
